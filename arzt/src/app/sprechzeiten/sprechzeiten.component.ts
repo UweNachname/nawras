8a3ff2985e9f0eb88426645ed0c0acb4844c7ab5
Uwe.Nachname <Uwe.Nachname@web.de>
2025-01-03 16:52:06 +0100

    Knopf

--- arzt/src/app/sprechzeiten/sprechzeiten.component.ts
+++ arzt/src/app/sprechzeiten/sprechzeiten.component.ts
@@ -9,5 +9,7 @@ import { CommonComponent } from '../shared/components/common..component';
   styleUrl: '../shared/css/common.component.css'
 })
 export class SprechzeitenComponent extends CommonComponent{
-
+  sendMail() {
+    alert('I am the action function for click event');
+ }
 }
